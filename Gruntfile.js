b0a4f9d361ca3209e0d5ffd0fe479ec01459a3e9
AsafGerchak <asaf.gerchak@gmail.com>
2014-08-07 21:16:07 -0400

    Initial porject push let'sfuckinDOTHISWOOOO :rocket:

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+
+ // Project configuration.
+ grunt.initConfig({
+   pkg: grunt.file.readJSON('package.json'),
+
+   watch: {
+   	css: {
+   		files: 'css/*.scss',
+   		tasks: ['sass', 'autoprefixer', 'recess']
+   	},
+   	html: {
+   		files: 'index.html',
+   		tasks: ['']
+   	},
+   	options: {
+   		livereload: true
+   	}
+   },
+
+   sass: {
+   	dist: {
+   		files: {
+   			'css/style.css' : 'css/style.scss'
+   		}
+   	}
+   },
+
+   autoprefixer: {
+   	options: {
+   		browsers: ['last 5 version', 'ie 8', 'ie 9']
+   	},
+   	no_dest: {
+   		src: 'css/style.css'
+   	}
+   },
+
+   connect: {
+   	server: {
+   		options: {
+   			port: '8000',
+   			base: ''
+   		}
+   	}
+   },
+
+   recess: {
+   	dist: {
+   		options: {
+   			compile: false,
+   			compress: true
+   		},
+   		files: {
+   			'css/mini.css' : 'css/style.css'
+   		}
+   	}
+   }
+
+ });
+
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-autoprefixer');
+grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.loadNpmTasks('grunt-recess');
+
+ // Default task(s).
+ grunt.registerTask('default', ['connect','watch']);
+};
